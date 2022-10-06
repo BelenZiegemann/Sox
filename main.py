c711c8cb9714d0fbb4ed18bfaa4a93cf8f84d803
Belen Ziegemann <beluziegemann@gmail.com>
2022-10-06 08:27:58 -0300

    agregue articulos

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ art1 = arbol.insert("", END, text="articulo1",values=("7","4","-3","12","0","500
 arbol.insert(art1, END, text="sub-articulo1", values=("-","-","-","10","15","?"))
 arbol.insert(art1, END, text="sub-articulo2", values=("10","10","10","5","20","?"))
 arbol.insert("", END, text="articulo2", values=("15","10","5","3","100","400"))
+art3 = arbol.insert("", END, text="articulo3",values=("7","4","-3","12","0","500"))
+arbol.insert(art3, END, text="sub-articulo1", values=("-","-","-","10","15","?"))
+arbol.insert(art3, END, text="sub-articulo2", values=("10","10","10","5","20","?"))
+arbol.insert("", END, text="articulo4", values=("15","10","5","3","100","400"))
+arbol.insert("", END, text="articulo5", values=("15","10","5","3","100","400"))
 
 arbol.heading("#0", text="Articulo")
 arbol.heading("Pedidos", text="Pedidos")
