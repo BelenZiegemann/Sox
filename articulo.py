736f3c7bd41c4972d3ee11f3e8cd8565a7152ad9
Belen Ziegemann <beluziegemann@gmail.com>
2022-11-11 19:40:22 -0300

    Modifique atributos para los articulos S.

--- articulo.py
+++ articulo.py
@@ -18,16 +18,18 @@ class ArticuloS(Articulo):
     hijos = []
     nietos = []
 
-    def __init__(self, cod, des, stockE, stockR, necesitaL, hijos, nietos):
+    def __init__(self, cod, des, stockE, stockR, necesitaL, hijos, nietos, fecha_prog, orden, cant, estado, max_bolsas_linea):
         super().__init__(cod, des,stockE)
         self.stockReservado = stockR
         self.necesita = necesitaL
         #self.necesitaTejeduria = necesitaT
         self.hijos = [hijos]
         self.nietos = [nietos]
-        #vendido, pedidos pendientes de entrega. Nuevas columnas. 
-        # articulo tenga stock. Que s, l y t lo hereden.  
-        #articulo s tenga stock expedicion. 
+        self.fecha = fecha_prog
+        self.orden = orden
+        self.cant = cant
+        self.estado = estado
+        self.max_bolsas = max_bolsas_linea
     
     def agregarHijo(self,hijo=[]):
         self.hijos.append(hijo)
@@ -57,8 +59,9 @@ class ArticuloT(Articulo):
         
 
 #Correcciones:
-#Modificar algunos atributos de los objetos (herencia). 
+#Modificar algunos atributos de los objetos (herencia). (Listo)
 #Agregar a la ventana principal columnas: vendido y pedidos a entregar. 
 #Agregar la nueva consulta.
 #Realizar una sola consulta a la base de datos.
 #Agregar atributos al articulo S: fecha de orden programada, cantidad, maximas bolsas posibles en linea.
+#Agregar max bolsas posibles en linea a la nueva consulta. 
